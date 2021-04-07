63148c882340b3e061112a2f720550e2b62f3058
Mengyuan Chen <chen.mengyuan@gmail.com>
2021-04-07 07:19:05 +0000

    modify the model without deleting the data

--- products/models.py
+++ products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
     description = models.TextField(blank = True, null = True)
     price       = models.DecimalField(decimal_places = 2, max_digits = 10000)
     summary     = models.TextField(default = 'This is cool')
+    feature     = models.BooleanField() # null = True, default = True
     
